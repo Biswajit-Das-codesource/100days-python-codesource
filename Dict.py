689f74287c591bd6c3ed6a4fe8105f04ed9fbd18
biswajit das <biswajit9348das@gmail.com>
2024-08-18 21:36:04 +0530

    dictionary

---
+++ Dict.py
@@ -0,0 +1,10 @@
+dict = {
+    "litindia" : "maut ka khel",
+    "DOG":"kutta",
+    "billi":"cat",
+    "beautiful":"sundar",
+    "eye":"akh"
+}
+word=input("enter the word to search : ")
+
+print(dict[word])
