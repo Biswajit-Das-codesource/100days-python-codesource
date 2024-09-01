e590824aad16c17ac91be5f3b315c8d3f38e7682
biswajit das <biswajit9348das@gmail.com>
2024-09-01 15:03:31 +0530

    area of triangle

---
+++ area_Triangle.py
@@ -0,0 +1,11 @@
+#find the area of the Triangle 
+
+import math # import math module for use square-root
+
+s1=int(input("enter the one side: "))
+s2=int(input("enter the second side: "))
+s3=int(input("enter the third side: "))
+
+s=(s1+s2+s3)/2 #find the s
+
+area=math.sqrt(s*s(s-s1)*(s-s2)*(s-s3)) #we use here math.sqrt for getting the square root of the number
