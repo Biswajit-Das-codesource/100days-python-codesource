a7599db723ed93654a5ce82600810380a12b0cbc
biswajit das <biswajit9348das@gmail.com>
2024-09-01 15:05:31 +0530

    palindrome

--- palindrome.py
+++ palindrome.py
@@ -1,9 +1,8 @@
 #Check the list is palindrome or not
 
-
 arr=[1,2,3,4,5,6]
-arr1=arr.copy()
-arr1.reverse()
+arr1=arr.copy() # .copy() method is used for copy the list 
+arr1.reverse() # .reverse() method is used for reverse the list
 print(arr1)
 if(arr==arr1):
     print("palindrome")
