ac65f457384ef0dabe12e3361242bfad371574db
biswajit das <biswajit9348das@gmail.com>
2024-09-01 12:10:51 +0530

    update

---
+++ palindrome.py
@@ -0,0 +1,28 @@
+#Check the list is palindrome or not
+
+
+arr=[1,2,3,4,5,6]
+arr1=arr.copy()
+arr1.reverse()
+print(arr1)
+if(arr==arr1):
+    print("palindrome")
+else: 
+    print("not palindrome")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
