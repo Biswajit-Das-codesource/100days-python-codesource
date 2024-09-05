34804afdd936558b8ae7a3b091f649d36d4ea62c
biswajit das <biswajit9348das@gmail.com>
2024-09-05 14:17:10 +0530

    set program

---
+++ join_set.py
@@ -0,0 +1,14 @@
+'''
+    Wap to find the union of the two sets & intersection of two sets
+'''
+
+set1={1,2,3,4,5}
+set2={5,6,7,8,9,10}
+
+join=set1.union(set2) #union method is used for join the two sets
+
+intersection=set1.intersection(set2) #intersection method is used for find the common element in between two sets 
+
+print(join) #print the union of the two sets
+
+print(intersection) #print the intersection of the two sets
